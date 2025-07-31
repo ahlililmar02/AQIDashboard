5e4f7329101a59e9b877a6eb79a3f256cef2a4b1
ahlililmar02 <136542395+ahlililmar02@users.noreply.github.com>
2025-07-31 22:54:14 +0700

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -86,9 +86,9 @@ df_today = df[df["time"] >= today]
 df_today_avg = df_today.groupby("station")[["aqi", "PM2.5"]].mean().reset_index()
 df_today_avg = df_today_avg[df_today_avg["aqi"].notna() & (df_today_avg["aqi"] != 0)]
 
-with open("static/style.css") as f:
+with open("streamlit/static/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-	
+
 if page == "Air Quality Monitor":
 	st.title("Real-Time Air Quality Dashboard")
 
