f7a2907e5c24727d715e1be7c539d0c2fe600e08
ahlililmar02 <136542395+ahlililmar02@users.noreply.github.com>
2025-07-31 13:01:44 +0700

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -530,7 +530,7 @@ elif page == "AOD Derived PM2.5 Heatmap":
 		heat_data = selected_df[["latitude", "longitude", "PM2.5"]].values.tolist()
 
 	# Map setup
-	m = folium.Map(location=[-6.2, 106.9], zoom_start=11, tiles="CartoDB positron")
+	m = folium.Map(location=[-6.2, 106.9], zoom_start=11, tiles="OpenStreetMap")
 
 	# Add heatmap layer
 	from folium.plugins import HeatMap
