3708dd4e76f49b89d1314b8afafe4538d635f994
ahlililmar02 <136542395+ahlililmar02@users.noreply.github.com>
2025-07-31 16:05:59 +0700

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -312,7 +312,6 @@ if page == "Air Quality Monitor":
 							padding:20px;
 							border-radius:12px;
 							box-shadow:0 2px 4px rgba(0,0,0,0.1);
-							text-align:center;
 						">
 							<h3 style='margin:0;'>AQI</h3>
 							<p style='font-size:24px;margin:0;'>{aqi_value:.0f}</p>
